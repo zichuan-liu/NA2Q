5b054518166df65459f1f3a1603b5e8591756bb9
Liu Zichuan <775269512@qq.com>
2024-01-14 17:27:11 +0800

    Add files via upload

--- src/envs/__init__.py
+++ src/envs/__init__.py
@@ -5,7 +5,14 @@ import sys
 import os
 from .stag_hunt import StagHunt
 from .foraging import ForagingEnv
+from .mpe.mpe_wrapper import MPEWrapper
 
+try:
+    gfootball = True
+    from .gfootball import GoogleFootballEnv
+except Exception as e:
+    gfootball = False
+    print(e)
 def env_fn(env, **kwargs) -> MultiAgentEnv:
     return env(**kwargs)
 
@@ -14,6 +21,10 @@ REGISTRY = {}
 REGISTRY["sc2"] = partial(env_fn, env=StarCraft2Env)
 REGISTRY["stag_hunt"] = partial(env_fn, env=StagHunt)
 REGISTRY["foraging"] = partial(env_fn, env=ForagingEnv)
+REGISTRY["mpe"] = partial(env_fn, env=MPEWrapper)
+
+if gfootball:
+    REGISTRY["gfootball"] = partial(env_fn, env=GoogleFootballEnv)
 if sys.platform == "linux":
     os.environ.setdefault("SC2PATH",
                           os.path.join(os.getcwd(), "3rdparty", "StarCraftII"))
